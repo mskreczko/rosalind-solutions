bef7eb3aa6626c7cea980497805b186ff88a70d1
mskreczko <mic.skreczko@gmail.com>
2023-02-06 08:46:52 +0100

    solution for finding a motif in dna

---
+++ solutions/finding_a_motif_in_dna.r
@@ -0,0 +1,13 @@
+findLocations <- function(dnaseq, pattern) {
+  locations <- c()
+  for (i in 1:(nchar(dnaseq) - nchar(pattern))) {
+    #print(substr(dnaseq, i, i + nchar(pattern) - 1))
+    if (substr(dnaseq, i, i + nchar(pattern) - 1) == pattern) {
+      locations <- c(locations, i)
+    }
+  }
+  return(locations)
+}
+
+data = readLines("../data/rosalind_subs.txt")
+findLocations(data[[1]], data[[2]])
